640cf14618b2f28764a115475d2a19e1a9918510
WireHallMedic <michaelwidler@yahoo.com>
2022-06-07 11:08:14 -0700

    specialize each loop

--- cherry/lib/main.rb
+++ cherry/lib/main.rb
@@ -4,18 +4,12 @@
 
 def total_and_smallest
   data = [23, 15, 8, 16, 4, 42]
-  smallest = data.first
   total = 0
   data.each do |d|
     total += d
-    if d < smallest
-      smallest = d
-    end
   end
   smallest = data.first
-  total = 0
   data.each do |d|
-    total += d
     if d < smallest
       smallest = d
     end
